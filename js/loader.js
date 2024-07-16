419a0003b6a4b4fad23d8b7feb6c3b3972900407
karthi12-dev <karthikaappu121@gmail.com>
2024-07-17 00:50:29 +0530

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,36 +1,64 @@
 $(window).on('load',function(){
 	if(localStorage.getItem('myNumber') == '' || localStorage.getItem('myNumber') == null){
-	$('#myModal').modal('show');
+		$('#myModal').modal('show');
 		$('#featured_property_section').hide();
 		$('#signout').css('display', 'none');
 		$('#loginuser').css('display', 'block');
-	}else{
-	$('#myModal').modal('hide');
+		}else{
+		$('#myModal').modal('hide');
 		$('#featured_property_section').show();
 		$('#signout').css('display', 'block');
 		$('#loginuser').css('display', 'none');
 	}
-	
 });
-	$('#loginuser').on('click', function(){
-		$('#myModal').modal('show');	
-		
-	});
-	$('#signout').on('click', function(){
-		localStorage.removeItem('myNumber');
-		location.reload();
-	});
-
+$('#loginuser').on('click', function(){
+	$('#myModal').modal('show');	
+});
+$('#signout').on('click', function(){
+	localStorage.removeItem('myNumber');
+	location.reload();
+});
 $('#generateotp').on('click', function(){
 	var mobile_num = $('#phone-num').val();
-	if (/^\d{10}$/.test(mobile_num)) {
-		localStorage.setItem('myNumber', mobile_num);
-		$('.mobile_field').css('display', 'none');
-		$('#featured_property_section').show();
-		$('.otp_field').css('display', 'block');
-		$('#signout').css('display', 'block');
-		$('#loginuser').css('display', 'none');
-		}else{
-		alert('Not a valid mobile num');
-	}
+	$.ajax({
+		url: './api/twilioApi.php',
+		type: 'POST',
+		data: {
+			functionname: 'sendOtp',
+			phnum: mobile_num
+		},
+		dataType:'json',
+		success: function(response){
+			var jsonResponse = JSON.parse(response);
+			localStorage.setItem('myNumber', mobile_num);
+			$('.mobile_field').css('display', 'none');
+			$('.otp_field').css('display', 'block');
+			$('#signout').css('display', 'block');
+			$('#loginuser').css('display', 'none');
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log(textStatus, errorThrown);
+		}
+	});
 });
+$('#verify_otp').on('click', function(e){
+	e.preventDefault();
+	var otp = $('#verify-otp').val();
+	var phone =  $('#phone-num').val();
+	$.ajax({
+		url: './api/twilioApi.php',
+		type: 'POST',
+		data: {
+			functionname: 'verifyOtp',
+			code: otp,
+			phnum: phone
+		},
+		success: function(response){
+			var jsonResponse = JSON.parse(response);
+			$('#featured_property_section').show();
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log(textStatus, errorThrown);
+		}
+	});
+}); 
